7f298ca4816f65c462de32fa6355d6e50a1cf9a6
veial1108 <vlados.99@mail.ru>
2025-10-21 15:22:25 +0500

    fix deploy and division into dockerfile prod and dev

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
+// vite.config.js
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'        // ← ЭТО ОБЯЗАТЕЛЬНО!
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,7 +9,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    // Указываем конфиг для Tailwind
     tailwindcss({
       config: './tailwind.config.js'
     }),
@@ -18,7 +18,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/vue-json-server-docker/',
+  base: '/', // ← для локальной разработки
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
